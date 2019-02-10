502179c744b064720dd6a1223eaa41ebce524993
Mvcampbell3 <mvcampbell3@gmail.com>
2019-02-09 19:57:10 -0500

    modified contructors

--- Letter.js
+++ Letter.js
@@ -11,8 +11,12 @@ function Letter (letter) {
     };
     
     this.check = function(userInput) {
+        console.log("checked");
         if (userInput === this.letter){
             this.guessed = true;
+            console.log("true check")
+        } else {
+            console.log("false check")
         }
     }
 }
--- Word.js
+++ Word.js
@@ -1,28 +1,28 @@
 let Letter = require("./Letter");
 
 function Word(word) {
-    this.word = word;
-    this.wordArray = this.word.split("");
-    this.stor = [];
-
-    this.con = function(){
-        console.log(this.wordArray);
-    }
-
-    this.makeLetter = function(){
-        this.wordArray.forEach((i) => {
-            i = new Letter(i);
-            this.stor.push(i);
-        });
-    }
+    
+    this.stor = word.split("").map((value) => new Letter(value))
 
     this.displayWord = function(){
-        this.makeLetter();
         let display = this.stor.map((v) => v.displayLetter());
         console.log(display.join(" "))
+    }
+
+    this.look = function(inp) {
+        letters = this.stor.map((filler) => filler.letter);
+        where = letters.indexOf(inp);
+        console.log(where);
+        if (where > -1) {
+            let fil = this.stor.filter((val) => val.letter === inp);
+            fil.forEach((one) => one.check(inp))
+            this.displayWord();
+        } else {
+            this.displayWord();
+        }
     }
 }
 
 let tennis = new Word("tennis");
-
-tennis.displayWord()
+// tennis.displayWord();
+tennis.look("t")
